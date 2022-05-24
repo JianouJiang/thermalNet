38365a323cf9b649e8dfae6f47401ed62b7c3619
JianouJiang <33976941+JianouJiang@users.noreply.github.com>
2022-05-24 12:51:36 +0100

    Update jianouTest.py

--- jianouTest.py
+++ jianouTest.py
@@ -11,7 +11,9 @@ def plotTest():
   predList_test_predictions = [9,8,7,6,5,4,3,2,1]
   plt.plot(pred_Benchmark, label='Benchmark')
   plt.plot(predList_test_predictions, label='thermalNet 3.0')
+  plt.xlabel('X-axis') 
   plt.legend(loc='upper right')
   plt.title('Normalised Thickness H Comparison')
+  fig.savefig('Normalised_Thickness_H_Comparison.pdf', bbox_inches='tight', dpi=150)
   plt.show()
   return
