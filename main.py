6889b608c8c988d31569db515d815c91aa88e0f0
JianouJiang <33976941+JianouJiang@users.noreply.github.com>
2022-05-24 15:48:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# the main code happens here!
+# we should just be running main.py to get any results that we want!
+
+def main():
+  
+  # run everything
+  print("---------main()----------")
+  return
