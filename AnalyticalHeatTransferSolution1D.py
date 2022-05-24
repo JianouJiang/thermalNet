4e7ef02e7ffc9f63eae53c53b6b1337f3bf12a71
JianouJiang <33976941+JianouJiang@users.noreply.github.com>
2022-05-24 16:17:48 +0100

    Create AnalyticalHeatTransferSolution1D.py

---
+++ AnalyticalHeatTransferSolution1D.py
@@ -0,0 +1,22 @@
+# Analytical Heat Transfer Solution in 1D
+
+# import related libs:
+
+
+# the initial condition:
+def unitPulse(x, t=0): 
+  
+  # TODO
+  print("---------unitPulse()-----")
+  
+  return
+
+# the analytical solution as a function of space and time, i.e. T(x,t):
+def Temperature_Analytical_1D(x,t): # input a single float value x and t
+  
+  # TODO
+  print("--------Temperature_Analytical_1D()---------")
+  # T = 
+  
+  
+  return T # returns a single value of T
