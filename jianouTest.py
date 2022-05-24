378c83dcec43046016ca4d5d7263f66e72751d90
JianouJiang <33976941+JianouJiang@users.noreply.github.com>
2022-05-24 12:45:39 +0100

    Update jianouTest.py

--- jianouTest.py
+++ jianouTest.py
@@ -1,3 +1,16 @@
+import matplotlib.pyplot as plt
+
+
 def jianouTest():
   print("My name is Jianou, good to have everybody here!")
+  return
+
+def plotTest():
+  pred_Benchmark = [1,2,3,4,5,6,7,8,9]
+  predList_test_predictions = [9,8,7,6,5,4,3,2,1]
+  plt.plot(pred_Benchmark, label='Benchmark')
+  plt.plot(predList_test_predictions, label='thermalNet 3.0')
+  plt.legend(loc='upper right')
+  plt.title('Normalised Thickness H Comparison')
+  plt.show()
   return
