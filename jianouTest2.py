c10ceeefa92196be5350956e92020003d617b825
JianouJiangOxford <jianou.jiang@eng.ox.ac.uk>
2022-05-24 14:31:34 +0100

    Update jianouTest2.py

--- jianouTest2.py
+++ jianouTest2.py
@@ -1,3 +1,4 @@
 def jianouTest2():
 	print("trying to push the first time as a complete new user in github from GitHub Desktop.")
+	print("the master of this branch should accept this change before it's actually updated.")
 	return
