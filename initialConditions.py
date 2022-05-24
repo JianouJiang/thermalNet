3eb27c0edaea0c63addde9ba03eac06ddb5357b7
JianouJiang <33976941+JianouJiang@users.noreply.github.com>
2022-05-24 16:41:25 +0100

    Update initialConditions.py

--- initialConditions.py
+++ initialConditions.py
@@ -1,5 +1,13 @@
 # setting up initial conditions:
 
+
+# but we need to generate meshes first:
+T = []
+rho = []
+Cp = []
+k = []
+
+
 def initialConditions(T, rho, Cp, k): # temperature, density, specific heat capacity and thermal conductivity
   
   # TODO
