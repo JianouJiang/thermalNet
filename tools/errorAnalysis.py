b09a77718e1b4fa8ffc7c623f99728087d2aad62
JianouJiang <33976941+JianouJiang@users.noreply.github.com>
2022-05-24 16:21:26 +0100

    Create errorAnalysis.py

---
+++ tools/errorAnalysis.py
@@ -0,0 +1,11 @@
+# error analysis function
+
+
+
+# this function aims to compare two lists of values
+def errorAnalysis(benchmark_solutions, approximated_solutions): # the two input lists must have the same dimension.
+  
+  # TODO
+  # Error_Sum = benchmark_solutions - approximated_solutions
+  # Error_avg = Error_Sum / len(benchmark_solutions)
+  return Error_avg
