7ddbf846280903418b85eea6397daef74581d09b
JianouJiang <33976941+JianouJiang@users.noreply.github.com>
2022-05-24 16:46:06 +0100

    Create materialProperties.py

---
+++ materialProperties.py
@@ -0,0 +1,34 @@
+# the material(s) properties such as rho, Cp, k might change as functions of T, so we should not treat them as constants
+
+
+# material 1: e.g. steel
+def rho_steel(T): # input as a temperature scalar field, e.g. 2D array
+  
+  return # returns a rho scalar field
+
+def Cp(T): # input as a temperature scalar field
+  
+  return
+
+
+def k(T):
+  
+  
+  return
+
+
+# material 2: e.g. coke or coal
+
+def rho(T):
+  
+  return
+
+def Cp(T):
+  
+  return
+
+
+def k(T):
+  
+  
+  return
