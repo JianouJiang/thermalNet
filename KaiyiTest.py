9b8140d72528e9e65f2ed7b6616352d3e881ead2
kydai1 <kd519@ic.ac.uk>
2022-06-06 12:51:39 +0100

    Update KaiyiTest.py

--- KaiyiTest.py
+++ KaiyiTest.py
@@ -1,4 +1,4 @@
-def KaiyiTest():
+def kaiyiTest():
     print("Howdy friends, my name is Kaiyi Dai, a 3rd Earth & Planetary science undergraduate student in IC.")
     print("I'm interested in Mars rover missions and don't hesitate to speak to me if you enjoy planetary stuff too!")
     return
