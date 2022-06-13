9c300796917494b0ad86155b733c9622f4b506d4
philip0802 <74053518+philip0802@users.noreply.github.com>
2022-06-13 22:11:25 +0800

    simply an introduction

---
+++ yanlinTest.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def yanlinTest():
+    print("Hey guys, I'm Yanlin or you may call me Philip, just graduated from Imperial College last year with Msci Physics.")
+    print("Currently I'm a system testing engineer at Huawei.")
+    return
