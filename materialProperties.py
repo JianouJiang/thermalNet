523ea5bd793e03e8bd59a014936fae625d0ab9bb
Jianou Jiang <33976941+JianouJiang@users.noreply.github.com>
2022-06-14 19:39:22 +0100

    finished the framework of material properties
    @philip0802 to fill in all the data for the four materials.

--- materialProperties.py
+++ materialProperties.py
@@ -1,34 +1,108 @@
-# the material(s) properties such as rho, Cp, k might change as functions of T, so we should not treat them as constants
+# the material(s) properties such as rho, Cp, k, h might change as functions of T, so we should not treat them as constants
 
+# using simple linear interpolation to get the corresponding coefficients
 
-# material 1: e.g. steel
-def rho_steel(T): # input as a temperature scalar field, e.g. 2D array
+# material 1: Aluminium
+# density
+def rho_Aluminium(T): # using simple linear interpolation to get the corresponding rho
   
-  return # returns a rho scalar field
-
-def Cp_steel(T): # input as a temperature scalar field
+  T_list = [300,400,500,600,700,800,900,1000,1100,1200] # 10 values here
+  rho_list = [...] # 10 values here as well
   
-  return
-
-
-def k_steel(T):
+  index = 0
+  for t in range(len(T_list)):
+    if T_list[i] <= T <= T_list[i+1]:
+      index = i
+      break
   
+  rho = (T - T_list[index])/100 * (rho_list[i+1] - rho_list[i])
   
-  return
+  return rho
 
-
-# material 2: e.g. coke or coal
-
-def rho_coke(T):
-  
-  return
-
-def Cp_coke(T):
+# specific heat capacity
+def Cp_Aluminium(T): 
+  # TODO
   
   return
 
 
-def k_coke(T):
+def k_Aluminium(T):
+  # TODO
+  
+  return
+
+# heat transfer coefficient
+def h_Aluminium(T):
+  # TODO
+  
+  return
+
+# TODO
+# material 2: Inconel 800HT
+
+def rho_Inconel800HT(T):
+  
+  return
+
+def Cp_Inconel800HT(T):
+  
+  return
+
+
+def k_Inconel800HT(T):
+  
+  
+  return
+
+def h_Inconel800HT(T):
+  
+  
+  return
+
+
+
+# TODO
+# material 3: Ethane Coke
+
+def rho_EthaneCoke(T):
+  
+  return
+
+def Cp_EthaneCoke(T):
+  
+  return
+
+
+def k_EthaneCoke(T):
+  
+  
+  return
+
+def h_EthaneCoke(T):
+  
+  
+  return
+
+
+
+# TODO
+# material 4: Naphtha Coke
+
+def rho_NaphthaCoke(T):
+  
+  return
+
+def Cp_NaphthaCoke(T):
+  
+  return
+
+
+def k_NaphthaCoke(T):
+  
+  
+  return
+
+def h_NaphthaCoke(T):
   
   
   return
