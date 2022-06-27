f5715a23d291b5ff86ef4126d31cbe83ca208204
Jianou Jiang <33976941+JianouJiang@users.noreply.github.com>
2022-06-27 16:02:36 +0100

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ def kaiyiTest():
     print("Howdy friends, my name is Kaiyi Dai, a 3rd Earth & Planetary science undergraduate student in IC.")
     print("I'm interested in Mars rover missions and don't hesitate to speak to me if you enjoy planetary stuff too!")
     return
+
+def xiaoyangTest():
+    print("Hello! I'm Sean, year 2 student studying EIE!")
+    print("I love playing guitar and are thrilled to work with you guys : )")
+    return
+
 def jianouTest():
   print("My name is Jianou, good to have everybody here! Testing plotTest() function below:")
   plotTest()
