6774d15df481a64b018ad82a652e67573e802264
Jianou Jiang <33976941+JianouJiang@users.noreply.github.com>
2022-06-27 16:01:41 +0100

    Delete KaiyiTest.py

--- KaiyiTest.py
+++
@@ -1,4 +0,0 @@
-def kaiyiTest():
-    print("Howdy friends, my name is Kaiyi Dai, a 3rd Earth & Planetary science undergraduate student in IC.")
-    print("I'm interested in Mars rover missions and don't hesitate to speak to me if you enjoy planetary stuff too!")
-    return
