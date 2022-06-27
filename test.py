e171800e17c6a3d96e0dc2062db0df3a9cd17597
Jianou Jiang <33976941+JianouJiang@users.noreply.github.com>
2022-06-27 16:00:11 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ from tools.normalisation import *
 
 # TODO!
 # from chennuoTest import * 
+def kaiyiTest():
+    print("Howdy friends, my name is Kaiyi Dai, a 3rd Earth & Planetary science undergraduate student in IC.")
+    print("I'm interested in Mars rover missions and don't hesitate to speak to me if you enjoy planetary stuff too!")
+    return
 
 print("Started testing all contributors...")
 def test():
