dcd53d9a7a0c9c76b7ae13dca30be1726dd386ed
JianouJiangOxford <jianou.jiang@eng.ox.ac.uk>
2022-06-27 13:16:51 +0100

    Delete jianouTest2.py

--- jianouTest2.py
+++
@@ -1,4 +0,0 @@
-def jianouTest2():
-	print("trying to push the first time as a complete new user in github from GitHub Desktop.")
-	print("the master of this branch should accept this change before it's actually updated.")
-	return
