c232144d4225b042df685c15658628a748944feb
Jianou Jiang <33976941+JianouJiang@users.noreply.github.com>
2022-06-29 12:17:42 +0100

    Update AnalyticalHeatTransferSolution1D.py

--- heat_transfer/AnalyticalHeatTransferSolution1D.py
+++ heat_transfer/AnalyticalHeatTransferSolution1D.py
@@ -2,12 +2,15 @@
 # with the initial conitions of unit pulse function, the double sine waves and the linear function.
 # importing libs:
 print("importing libs")
+# importing sys
+import sys
+sys.path.insert(0, '../tools/')
 import numpy as np
 from math import *
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 import os
-from tools.readParameters import *
+from tools import *
 
 # setting parameters:
 print("setting parameters")
@@ -108,31 +111,7 @@ def _lambda(x): # thermal diffusivity is related to space (e.g. steel for x<0.5 
 
 _lambda_list = _lambda(x)
 
-# tool box:
-# integrating from start to end
-def integral(f): 
-    
-    sum_fx = 0
-    for i in range(0,(len(f)-1)):
-        fi = (f[i]+f[i+1] )/2 # central differencing
-        sum_fx = sum_fx + fi*dx
-    return sum_fx
-    
-# open file in write mode and write data
-def writeData(directory, ti, T, _lambda): # note T and lambda here are lists, whereas time t is single value
-    isFile = os.path.isfile(directory) 
-    with open(directory, 'a') as fp:
-        index = 0
-        for Ti in T:
-            xi = 0 + dx * index
-            _lambda_i = _lambda[index]
-            # write each ti, xi, Ti, lambda on a new line
-            line = str(ti) + " " + str(xi) + " " + str(Ti) + " " + str(_lambda_i) + "\n"
-            fp.write(line)
-            index = index + 1
-    return
-    
-    
+   
     
 # deriving analytical solutions:
 print("deriving analytical solutions")
