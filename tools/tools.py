5db3c95eafcd924281b990ecdc3eb47937072874
Jianou Jiang <33976941+JianouJiang@users.noreply.github.com>
2022-06-29 12:16:50 +0100

    Update tools.py

--- tools/tools.py
+++ tools/tools.py
@@ -23,3 +23,14 @@ def writeData(directory, ti, T, _lambda): # note T and lambda here are lists, wh
     return
     
  
+# read parameters
+def readParameters(parameters_directory):
+  parameter_list = []  
+  with open(parameters_directory) as fp:
+    Lines = fp.readlines()
+    for line in Lines:
+        print(line)
+      parameter_list.append(line[2])     
+  L, dx, t_max, dt, _lambda1, _lambda2 = parameter_list[0], parameter_list[1], parameter_list[2], parameter_list[3], parameter_list[4], parameter_list[5]
+  print("L="+str(L)+" dx="+str(dx)+" t_max="+str(t_max)+" dt="+str(dt) + " _lambda1="+str(_lambda1)+" _lambda2="+str(_lambda2))
+  return L, dx, t_max, dt, _lambda1, _lambda2
