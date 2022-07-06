57eaa9fb8aaad43bd2ade4897cadd24cadc20048
INOMAD17 <Evelynnn17@hotmail.com>
2022-07-06 11:35:51 -0400

    evolveT sines mixed aluminium
    evolveT sines mixed aluminium

--- heat_transfer/examples/evolveT_1D_Mixed_Sines_Aluminium.py
+++ heat_transfer/examples/evolveT_1D_Mixed_Sines_Aluminium.py
@@ -1,36 +1,36 @@
+# Chennuo: EvolveT 1D Mixed Sines Aluminium
 
-# chennuo: EvolveT_1D_Dirichlet_Sines_Aluminium
-
-# import libs
+# import libs 
 print("import libs")
 import sys
-sys.path.insert(0, '../../')
+sys.path.insert(0, '../../../')
 import numpy as np
 from heat_transfer.initialConditions import *
 from heat_transfer.boundaryConditions import *
 from heat_transfer.linearSystemSolvers import *
 from heat_transfer.crankNicolson import *
-from heat_transfer.materialProperties import _lambda_Aluminium
+from heat_transfer.materialProperties import *
 
 from tools.tools import *
 parameters_directory="../parameters.txt"
 L, dx, t_max, dt, _lambda1, _lambda2, number_of_ghost_points, num_of_timeSteps_for_plotting = readParameters(parameters_directory)
 
 # evolve temperature
-print("started evolveT_1D_Dirichlet_Convec_Linear0_Aluminium().")
-def evolveT_1D_Dirichlet_Convec_Linear0_Aluminium():
-  directory = "../../data/crankNicolson_linear0_Dirichlet_Convec_Aluminium_T.txt"
+print("started evolveT_1D__Mixed_Sines_Aluminium().")
+def evolveT_1D_Mixed_Sines_Aluminium():
+  directory = "../../data/crankNicolson_Sines_Mixed_Aluminium_T.txt"
+
   # importing initial conditions
-  t, x, T, mask, _lambda = IC_1D_Linear0_TwoMaterials()
+  t, x, T, mask, _lambda = IC_1D_Sines_Aluminium()
   for i in range(len(t)):
     ti = t[i]
     # making boundary conditions
-    T, _lambda = BC_1D_Dirichlet_Tbl500_Convection(T, x, _lambda, mask)
+    T = BC_1D_Mixed(T, x, mask)
     
     # saving Temperature at t=n to .txt under /data
     writeData(directory, ti, x, T, _lambda)
     
-    A, b = crankNicolson1D_Dirichlet(T, mask, _lambda, dx, dt)
+    A, b = crankNicolson1D_Mixed(T, mask, _lambda, dx, dt)
     Tn, residual = SOR(A, b, x0=None, N=100, r=10 ** -6, w=1.5)
     #print(Tn)
     
@@ -39,9 +39,9 @@ def evolveT_1D_Dirichlet_Convec_Linear0_Aluminium():
     # getting the lambda based on the newly obtained temperature
     for i in range(len(T)):
       Ti = T[i]
-      _lambda[3][i] = _lambda_Aluminium(Ti)
+      _lambda[i] = 1 #_lambda_Aluminium(Ti)
   
   return
 
-evolveT_1D_Dirichlet_Convec_Linear0_Aluminium()
-print("finished evolveT_1D_Dirichlet_Convec_Linear0_Aluminium().")
+evolveT_1D_Mixed_Sines_Aluminium()
+print("finished evolveT_1D_Mixed_Sines_Aluminium().")
