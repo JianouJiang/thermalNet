478dfa638b770075b34257264cb0bbe484467332
Jianou Jiang <33976941+JianouJiang@users.noreply.github.com>
2022-07-10 13:52:49 +0100

    restructured convertData.py
    restructured convertData.py

--- ansys/convertData.py
+++ ansys/convertData.py
@@ -2,13 +2,13 @@
 print("convertCSVtoTxT()")
 import time
 import sys
-sys.path.insert(0, '../../')
+sys.path.insert(0, '../')
 import numpy as np
 from tools.tools import *
 
 
-directory_read="Al_constant IC_left0_right1000.csv"
-directory_write = "../../data/ansys_linear0_Dirichlet_Aluminium_T.txt"
+directory_read="Al_uniform_IC_BC_left0_right1000/Al_constant_IC_left0_right1000.csv"
+directory_write = "../data/ansys_linear0_Dirichlet_Aluminium_T.txt"
 
 
 convertCSVtoTxT(directory_read, directory_write)
